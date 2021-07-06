1e6849cb60354cead86cd54a2e92773804e8ecd1
lancebrudnicki <lancebrudnicki@gmail.com>
2021-07-06 14:17:57 -0600

    July_2021

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
-    
-    
-class SomeClassComponent extends Component {
-    render() {
-    const {firstname,lastname,age,haircolor} = this.props;
-        return (
-<div>
-        <h1>{lastname}, {firstname}</h1>
-        <p>age: {age}</p>
-        <p>haircolor: {haircolor}</p>
-</div>
-);
-    }
+import React from 'react';
+
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>{ props.lastname }, { props.firstname }</h1>
+            <p>Age: { props.age }</p>
+            <p>Hair Color: { props.hairColor }</p>
+        </div>
+    );
 }
-    
-export default SomeClassComponent;
+export default PersonCard;
+
